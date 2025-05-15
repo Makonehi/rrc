27d954388c76849b01bff921890923ed66502603
lekot <lekotim05.2006@gmail.com>
2025-05-15 21:06:39 +0300

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,8 +38,8 @@ export default {
             zero: 'rgb(0, 0, 0)',
         },
         screens: {
-            // xs: "375px",
-            // ss: "480px",
+            xs: "375px",
+            ss: "480px",
             sm: "640px",
             md: "768px",
             lg: "1024px",
